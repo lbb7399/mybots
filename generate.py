150498ad12a725097f97053a0f0547a7ef880926
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-09 13:57:59 -0600

    stacked reducing tower

--- generate.py
+++ generate.py
@@ -1,4 +1,17 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+length0 = 1
+width0 = 1
+height0 = 1
+x = 2.5
+y = 2.5
+z0 = 0.5
+for i in range(10):
+    z = z0 + i
+    length = length0*(0.9**i)
+    width = width0*(0.9**i)
+    height = height0*(0.9**i)
+    
+    pyrosim.Send_Cube(name= f"Box{i}", pos=[x,y,z] , size=[length,width,height])
+
 pyrosim.End()
