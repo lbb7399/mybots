97bc4943530a0cb55a2ec0a4e6911e5dfc9d1804
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-09 18:14:08 -0600

    one block, world.sdf

--- generate.py
+++ generate.py
@@ -1,21 +1,10 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 length0 = 1
 width0 = 1
 height0 = 1
-x0 = -2
-y0 = -2
-z0 = 0.5
-for k in range(5):
-    x = x0 + k
-    for j in range(5):
-        y = y0 + j
-        for i in range(10):
-            z = z0 + i
-            length = length0*(0.9**i)
-            width = width0*(0.9**i)
-            height = height0*(0.9**i)
-            
-            pyrosim.Send_Cube(name= f"Box{k}{j}{i}", pos=[x,y,z] , size=[length,width,height])
-
+x = 0
+y = 0
+z = 0.5
+pyrosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length,width,height])
 pyrosim.End()
