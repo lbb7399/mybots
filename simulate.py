b750d763045f1c6cf505bc6ba5f477dd06298676
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-09 18:48:41 -0600

    world and robot

--- simulate.py
+++ simulate.py
@@ -5,6 +5,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
