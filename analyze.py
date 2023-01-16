1c7e2537d85a0135be22940e0af663f2aca73339
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 14:54:20 -0600

    targetAngles plot

--- analyze.py
+++ analyze.py
@@ -1,10 +1,12 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
-backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
-frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+backLegSensorValues = np.load('data/backLegSensorValues.npy')
+frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+targetAngles = np.load('data/targetAngles.npy')
 
-plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 4)
-plt.plot(frontLegSensorValues, label = "Front Leg")
-plt.legend()
+#plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 4)
+#plt.plot(frontLegSensorValues, label = "Front Leg")
+#plt.legend()
+plt.plot(targetAngles)
 plt.show()
