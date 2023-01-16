1185df0cf12cf0a59bfa6a06b5f8f3f03bb833bc
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 14:21:45 -0600

    added motor to back leg joint

--- simulate.py
+++ simulate.py
@@ -17,6 +17,9 @@ for i in range(stepsiter):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+    
     time.sleep(1/60)
 backfilename = 'data/backLegSensorValues.npy'
 numpy.save(backfilename, backLegSensorValues)
