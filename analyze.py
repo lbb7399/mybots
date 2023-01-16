47995b38f34b48fce76476c32be9f37c1aa98f6f
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 15:53:08 -0600

    dif motor contorls for each leg plot

--- analyze.py
+++ analyze.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-backLegSensorValues = np.load('data/backLegSensorValues.npy')
-frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
-targetAngles = np.load('data/targetAngles.npy')
-
+#backLegSensorValues = np.load('data/backLegSensorValues.npy')
+#frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+#targetAngles = np.load('data/targetAngles.npy')
+targetAnglesBack  = np.load('data/targetAnglesBack.npy')
+targetAnglesFront = np.load('data/targetAnglesFront.npy')
 #plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 4)
 #plt.plot(frontLegSensorValues, label = "Front Leg")
-#plt.legend()
-plt.plot(targetAngles)
+
+#plt.plot(targetAngles)
+plt.plot(targetAnglesBack, label = "Back Leg", linewidth = 4)
+plt.plot(targetAnglesFront, label = "Front Leg")
+plt.legend()
 plt.show()
