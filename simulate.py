1e6d8ec86254f1df44f585f44c356fe74ef31e53
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 14:33:10 -0600

    tip toes

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
-import numpy
+import numpy as np
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -10,20 +10,21 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-stepsiter = 100
-backLegSensorValues = numpy.zeros(stepsiter)
-frontLegSensorValues = numpy.zeros(stepsiter)
+stepsiter = 1000
+backLegSensorValues = np.zeros(stepsiter)
+frontLegSensorValues = np.zeros(stepsiter)
 for i in range(stepsiter):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = -np.pi/4.0, maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = np.pi/4.0, maxForce = 500)
     
     time.sleep(1/60)
 backfilename = 'data/backLegSensorValues.npy'
-numpy.save(backfilename, backLegSensorValues)
+np.save(backfilename, backLegSensorValues)
 frontfilename = 'data/frontLegSensorValues.npy'
-numpy.save(frontfilename, frontLegSensorValues)
+np.save(frontfilename, frontLegSensorValues)
 p.disconnect()
 
