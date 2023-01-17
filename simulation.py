2bf30e4c55a5483677f3d7356435fd2b0448b40f
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 20:03:06 -0600

    simulation class

---
+++ simulation.py
@@ -0,0 +1,30 @@
+from world import WORLD
+from robot import ROBOT
+import pybullet as p
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import time
+import constants as c
+class SIMULATION:
+    def __init__(self):
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8)
+        self.world = WORLD()
+        self.robot = ROBOT()
+        pyrosim.Prepare_To_Simulate(self.robot.robotId)
+        self.robot.Prepare_To_Sense()
+        self.robot.Prepare_To_Act()
+    def Run(self):
+        for i in range(c.stepsiter):
+            p.stepSimulation()
+            self.robot.Sense(i)
+            self.robot.Act(i)
+
+        #
+        #    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesBack[i], maxForce = c.maxForceBack)
+        #    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesFront[i], maxForce = c.maxForceFront)
+            #print(i)
+            time.sleep(1/240)
+    def __del__(self):
+        p.disconnect()
