486b1140bf557ec072894a3dfc05273c64069b47
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-16 20:00:21 -0600

    first round of constants

---
+++ constants.py
@@ -0,0 +1,14 @@
+import numpy as np
+stepsiter = 100
+
+amplitudeBack = 4*np.pi/16
+frequencyBack = 10
+phaseOffsetBack = np.pi/8
+maxForceBack = 40
+
+amplitudeFront = 4*np.pi/16
+frequencyFront = 10
+phaseOffsetFront = 0
+maxForceFront = 40
+
+
