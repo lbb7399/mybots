7a462c3d98dcd25e9102a96b66dc02a2defa0318
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-21 17:19:39 -0600

    simulating sensor neurons

--- generate.py
+++ generate.py
@@ -12,25 +12,28 @@ def Create_World():
     pyrosim.End()
     return
     
-def Create_Robot():
+def Generate_Body():
     length = 1
     width = 1
     height = 1
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name= "Link0", pos=[0,0,0.5] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
-    pyrosim.Send_Cube(name= "Link1", pos=[0,0,0.5] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
-    pyrosim.Send_Cube(name= "Link2", pos=[0,0,0.5] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
-    pyrosim.Send_Cube(name= "Link3", pos=[0,0.5,0] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,1,0])
-    pyrosim.Send_Cube(name= "Link4", pos=[0,0.5,0] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0,0.5,-0.5])
-    pyrosim.Send_Cube(name= "Link5", pos=[0,0,-0.5] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1])
-    pyrosim.Send_Cube(name= "Link6", pos=[0,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Cube(name= "Torso", pos=[1.5,0,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name= "BackLeg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name= "FrontLeg", pos=[0.5,0,-0.5] , size=[length,width,height])
     pyrosim.End()
     return
+
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+    pyrosim.End()
+    return
+
+
 Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
