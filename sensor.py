af724ad01ff8689c16c632ddcd0f80557e47700e
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-21 17:22:56 -0600

    simulating sensor neurons

--- sensor.py
+++ sensor.py
@@ -8,8 +8,7 @@ class SENSOR:
     
     def Get_Value(self, iter):
         self.values[iter] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
-        #if iter == c.stepsiter - 1:
-           # print(self.values)
+
     def Save_Values(self):
         filename = f"data/{self.linkName}SensorValues.npy"
         np.save(filename, self.values)
