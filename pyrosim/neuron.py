b8716cfdd51255d22087d9f737ae3505a239b95a
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-21 18:10:50 -0600

    hidden and motor neurons

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -124,3 +124,6 @@ class NEURON:
         
     def Update_Sensor_Neuron(self):
         self.Set_Value = pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name())
+    
+    def Update_Hidden_Or_Motor_Neuron(self):
+            self.Set_Value = 0
