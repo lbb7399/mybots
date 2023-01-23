3e17f4da5f9a9a77be7ef83555b319e4424a36fb
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-23 12:33:44 -0600

    removed prepare to act and save values

--- motor.py
+++ motor.py
@@ -6,19 +6,9 @@ import pybullet as p
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-    
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitudeBack
-        self.offset = c.phaseOffsetBack
-        if self.jointName == "Torso_BackLeg":
-            self.frequency = c.frequencyBack
-        else:
-            self.frequency = c.frequencyBack/2
-        self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(0, 2*np.pi, c.stepsiter) + self.offset)
+
         
     def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = desiredAngle, maxForce = c.maxForceBack)
         
-    def Save_Values(self):
-        filename = f"data/{self.jointName}MotorValues.npy"
-        np.save(filename, self.MotorValues)
+
