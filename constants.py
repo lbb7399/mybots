b999c0fa86b283158ece93a844bc744f78a6d3bc
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-23 14:01:29 -0600

    changed max force and frequency

--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
 import numpy as np
-stepsiter = 100
+stepsiter = 1000
 
 amplitudeBack = 4*np.pi/16
 frequencyBack = 10
-phaseOffsetBack = np.pi/8
-maxForceBack = 40
+phaseOffsetBack = 0
+maxForceBack = 30
 
 amplitudeFront = 4*np.pi/16
 frequencyFront = 10
 phaseOffsetFront = 0
-maxForceFront = 40
+maxForceFront = 30
 
 
