cf46b4a0fba26fc77503e6b58ef64c6f79d5c7ae
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-23 12:15:15 -0600

    open to almost closed loop control

--- motor.py
+++ motor.py
@@ -9,12 +9,15 @@ class MOTOR:
     
     def Prepare_To_Act(self):
         self.amplitude = c.amplitudeBack
-        self.frequency = c.frequencyBack
         self.offset = c.phaseOffsetBack
+        if self.jointName == "Torso_BackLeg":
+            self.frequency = c.frequencyBack
+        else:
+            self.frequency = c.frequencyBack/2
         self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(0, 2*np.pi, c.stepsiter) + self.offset)
         
-    def Set_Value(self, robotId, iter):
-        pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = self.motorValues[iter], maxForce = c.maxForceBack)
+    def Set_Value(self, robotId, desiredAngle):
+        pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = desiredAngle, maxForce = c.maxForceBack)
         
     def Save_Values(self):
         filename = f"data/{self.jointName}MotorValues.npy"
