2db5c8e7047bd2c4e16b99f79a68c03ca3170ba0
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-23 13:07:56 -0600

    add synapse

--- generate.py
+++ generate.py
@@ -32,6 +32,7 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
     pyrosim.End()
     return
 
