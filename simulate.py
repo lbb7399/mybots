99ca977ceabc63ffa82f15ac5b262372c18c6b42
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-28 15:02:49 -0600

    show evolved behavior

--- simulate.py
+++ simulate.py
@@ -1,8 +1,10 @@
-
+import sys
 from simulation import SIMULATION
 
+directOrGUI = sys.argv[1]
 
+#print(directOrGUI)
 
-simulation = SIMULATION()
+simulation = SIMULATION(directOrGUI)
 simulation.Run()
 simulation.Get_Fitness()
