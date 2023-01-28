fbad80f8fcec48647719ee28314f8797d0a9e5bc
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-28 17:48:47 -0600

    fitness files

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -1,8 +1,14 @@
 from solution import SOLUTION
 import constants as c
 import copy
+import os
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
+        
+        os.system("rm brain*.nndf")
+        os.system("rm fitness*.txt")
+        
+        
         self.parents = {}
         self.nextAvailableID = 0
         for i in range(c.populationSize):
@@ -12,18 +18,22 @@ class PARALLEL_HILL_CLIMBER:
     
     def Evolve(self):
         for i in range(c.populationSize):
-            self.parents[i].Evaluate("GUI")
+            self.parents[i].Start_Simulation("DIRECT")
+            
+        for i in range(c.populationSize):
+            self.parents[i].Wait_For_Simulation_To_End()
         
-        #for currentGeneration in range(c.numberOfGenerations):
-            #self.Evolve_For_One_Generation()
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
 
             
     def Evolve_For_One_Generation(self):
-        self.Spawn()
-        self.Mutate()
-        self.child.Evaluate("DIRECT")
-        print(f"Parent fit: {self.parent.fitness}, Child fit: {self.child.fitness}")
-        self.Select()
+#        self.Spawn()
+#        self.Mutate()
+#        self.child.Evaluate("DIRECT")
+#        print(f"Parent fit: {self.parent.fitness}, Child fit: {self.child.fitness}")
+#        self.Select()
+        pass
     
     def Spawn(self):
         self.child = copy.deepcopy(self.parent)
