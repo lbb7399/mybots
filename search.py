3c8c8d8057d125f20853e24d64b66ea0b1662839
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-28 15:30:00 -0600

    now phc not hc

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
-from hillclimber import HILL_CLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
-hc = HILL_CLIMBER()
-hc.Evolve()
-hc.Show_Best()
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
