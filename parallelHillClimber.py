4c5692c67c6292dc6eb6f39ccf49f11027ac6dd5
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-29 18:08:33 -0600

    Spawn, Mutate, Evaluate

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -17,36 +17,47 @@ class PARALLEL_HILL_CLIMBER:
 
     
     def Evolve(self):
-        for i in range(c.populationSize):
-            self.parents[i].Start_Simulation("DIRECT")
-            
-        for i in range(c.populationSize):
-            self.parents[i].Wait_For_Simulation_To_End()
+#        for i in range(c.populationSize):
+#            self.parents[i].Start_Simulation("DIRECT")
+#
+#        for i in range(c.populationSize):
+#            self.parents[i].Wait_For_Simulation_To_End()
+
+        # instead:
+        self.Evaluate(self.parents)
+        
         
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
 
             
     def Evolve_For_One_Generation(self):
-#        self.Spawn()
-#        self.Mutate()
-#        self.child.Evaluate("DIRECT")
+        self.Spawn()
+        self.Mutate()
+#        self.child.Evaluate("DIRECT") #old
+        self.Evaluate(self.children)
+    
 #        print(f"Parent fit: {self.parent.fitness}, Child fit: {self.child.fitness}")
 #        self.Select()
-        pass
+        
     
     def Spawn(self):
-        self.child = copy.deepcopy(self.parent)
-        self.child.SET_ID(self.nextAvailableID)
-        self.nextAvailableID = self.nextAvailableID + 1
-        
+        self.children = {}
+        for key in self.parents:
+            self.children[key] = copy.deepcopy(self.parents[key])
+            self.children[key].SET_ID(self.nextAvailableID)
+            self.nextAvailableID = self.nextAvailableID + 1
         
     def Mutate(self):
-        self.child.Mutate()
-#        print("Parent:")
-#        print(self.parent.weights)
-#        print("Child:")
-#        print(self.child.weights)
+        for key in self.children:
+            self.children[key].Mutate()
+            
+    def Evaluate(self, solutions):
+        for i in range(c.populationSize):
+            solutions[i].Start_Simulation("DIRECT")
+            
+        for i in range(c.populationSize):
+            solutions[i].Wait_For_Simulation_To_End()
     
     def Select(self):
         if self.child.fitness > self.parent.fitness:
