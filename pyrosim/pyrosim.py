862b2d303318cf5c8af27f2db8843be94cbb2f10
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-01-29 21:09:25 -0600

    quadruped

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -134,11 +134,11 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
 
     availableLinkIndex = availableLinkIndex + 1
 
-def Send_Joint(name,parent,child,type,position):
+def Send_Joint(name,parent,child,type,position, jointAxis):
 
-    joint = JOINT(name,parent,child,type,position)
+    joint = JOINT(name,parent,child,type,position,jointAxis)
 
-    joint.Save(f)
+    joint.Save(f, jointAxis)
 
 def Send_Motor_Neuron(name,jointName):
 
