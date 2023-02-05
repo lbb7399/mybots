9f50aaa333a0f86a962fa09ebf457211df947ebc
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-02-05 17:12:13 -0600

    added sphere

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -106,7 +106,7 @@ def Prepare_To_Simulate(bodyID):
 
     Prepare_Joint_Dictionary(bodyID)
 
-def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
+def Send_Link(name,pos,size,objectType):
 
     global availableLinkIndex
 
@@ -116,7 +116,7 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
 
         Start_Model(name,pos)
 
-        link = LINK_SDF(name,pos,size)
+        link = LINK_SDF(name,pos,size,objectType)
 
         links.append(link)
     else:
@@ -133,6 +133,13 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
     linkNamesToIndices[name] = availableLinkIndex
 
     availableLinkIndex = availableLinkIndex + 1
+    
+# next two added by me
+def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
+    Send_Link(name,pos,size,"box")
+    
+def Send_Sphere(name="default",pos=[0,0,0],size=[0.5]):
+    Send_Link(name,pos,size,"sphere")
 
 def Send_Joint(name,parent,child,type,position, jointAxis):
 
