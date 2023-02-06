9608225d3a80befc704a9a9a72ad529c8f2c5d9b
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-02-06 15:00:21 -0600

    added time

--- search.py
+++ search.py
@@ -1,5 +1,10 @@
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
+import time
 
+start_time = time.time()
 phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
+end_time = time.time()
+
+print(end_time- start_time)
