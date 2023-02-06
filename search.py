95a8041617add4b58eac12050168aa88f669485f
Lindsay Bogar <lindsaybogar@Lindsays-MacBook-Pro-3.local>
2023-02-06 16:58:47 -0600

    multiple phc

--- search.py
+++ search.py
@@ -1,10 +1,15 @@
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import time
+import constants as c
 
 start_time = time.time()
-phc = PARALLEL_HILL_CLIMBER()
-phc.Evolve()
-phc.Show_Best()
+phcs = {}
+for i in range(c.bodyGen):
+    phcs[i] = PARALLEL_HILL_CLIMBER()
+    phcs[i].Evolve()
+    phcs[i].Show_Best()
+
+
 end_time = time.time()
 
 print(end_time- start_time)
